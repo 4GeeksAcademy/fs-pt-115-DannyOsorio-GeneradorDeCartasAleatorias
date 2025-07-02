698ddd38c51b78691afad6722ba895f7271d6920
OsorioDanny <daosva97@gmail.com>
2025-07-02 19:33:33 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,42 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  
+let palo = ['♥', '♣', '♦', '♠'];
+let numero = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+console.log(numero);
+console.log(palo);
+
+let paloAleatorio = palo[Math.floor(Math.random() * palo.length)];
+console.log(paloAleatorio);
+
+let numeroAleatorio = numero[Math.floor(Math.random() * numero.length)];
+console.log(numeroAleatorio);
+
+let icono1 = document.querySelector('.palo1');
+let icono2 = document.querySelector('.palo2');
+let numeroCarta = document.querySelector('#numero');
+
+icono1.innerHTML = paloAleatorio;
+icono2.innerHTML = paloAleatorio;
+numeroCarta.innerHTML = numeroAleatorio;
+
+if (paloAleatorio === '♥' || paloAleatorio === '♦') {
+    icono1.style.color = 'red';
+    icono2.style.color = 'red';
+} else {
+    icono1.style.color = 'black';
+    icono2.style.color = 'black';
+}
+
+if (numeroAleatorio === '♥' || numeroAleatorio === '♦') {
+    numeroCarta.style.color = 'red';
+
+} else {
+    numeroCarta.style.color = 'black';
+}
+
+
 };
